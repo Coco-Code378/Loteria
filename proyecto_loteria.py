8b1a0bd680c3feddd0c87de0e808cd2daf98b413
Matias <christian.matias@dewey.edu>
2025-03-19 17:46:23 -0400

    Restructura del programa
    En este commit, restructuré el programa para que fuera más ordenado. Además, realicé funciones para la creación de sorteos de Saca3.

--- proyecto_loteria.py
+++ proyecto_loteria.py
@@ -37,10 +37,12 @@ def limpiar_pantalla():
 # Fin de la función Limpiar_Pantalla
 
 #-------------------------------------------------------------------------------------------------------
-#Funcion para la creación de boletas
-boleta_total = []
+#Funcion Principal de creación de boletas
+
 def creacion_boletas_loteria():
-   
+    boleta_total = []
+#Funcion Secundaria para Aleatorio
+#---------------------------------------------------------------------------------------------------------
     def aleatorio():
          boletas_aleatoria = []
          for i in range(6):
@@ -49,62 +51,106 @@ def creacion_boletas_loteria():
              boletas_aleatoria.append(boleta)
          print(f"Esta es tu boleta aleatoria: {boletas_aleatoria}")
          return boletas_aleatoria
-     
+ #------------------------------------------------------------------------------------------------------------
+ # Fin de la función Aleatoria
+   
+#Función Secundaria para Manual
 #-----------------------------------------------------------------------------------------------------------
     def manual():
+        #Variables y listas
         cantidad_jugadas = int(input("Ingrese la cantidad de jugadas a realizar: "))
 
         jugadas = []
-        for i in range(cantidad_jugadas):
-            valores_jugadas = []
-            valores_unicos = set()
-    
-            for j in range(6):
-                while True:  
-                    numeros = int(input(f"Ingrese un valor: {j + 1} del 0 al {66 - 1} en la jugada {i + 1}, (No se puede repetir): "))
+        while True:
+            try:
+                for i in range(cantidad_jugadas):
+                    valores_jugadas = []
+                    valores_unicos = set()
+        
+                    for j in range(6):
+                        while True:
+                            numeros = int(input(f"Ingrese un valor: {j + 1} del 0 al {66 - 1} en la jugada {i + 1}, (No se puede repetir): "))
+                    
+                            if 0 <=  numeros <= 65:
+                                if numeros not in valores_unicos:
+                                    print(f"\n El número: {numeros} fue agregado \n")
+                                    valores_jugadas.append(numeros)
+                                    valores_unicos.add(numeros)
+                                    break  
+                                else:
+                                    print(f"\n No se puede repetir el valor {numeros}, por favor inténtelo de nuevo")
+                            else:
+                                print("\n Elegiste un número no correspondido a lo que se pide.")    
+                    valores_unicos.clear()             
+                    jugadas.append(valores_jugadas)
+                    boleta_total.append(valores_jugadas)
                 
-                    if 0 <=  numeros <= 65:
-                        if numeros not in valores_unicos:
-                            print(f"El número: {numeros} fue agregado")
-                            valores_jugadas.append(numeros)
-                            valores_unicos.add(numeros)
-                            break  
-                        else:
-                            print(f"No se puede repetir el valor {numeros}, por favor inténtelo de nuevo")
-                    else:
-                        print("Elegiste un número no correspondido a lo que se pide.")    
-            valores_unicos.clear()             
-            jugadas.append(valores_jugadas)
-            boleta_total.append(valores_jugadas)
-            
-        print(f"Boletas manuales ingresadas: {jugadas}")
-    return aleatorio, manual
+                print(f"Boletas manuales ingresadas: {jugadas}")
+                break
+            except ValueError as value:
+                print(f"\n Ingresaste un valor diferente a lo que se pide, por favor, inténtelo de nuevo. Error: {value} ")
+    #--------------------------------------------------------------------------------------------------------------------------------------------------
+    #Fin de la función Manual
+    
+    #Returns de la función Creación_Boletas:
+    return aleatorio, manual, boleta_total
+#-------------------------------------------------------------------------------------------------------------------------------------------------------
+#Fin de la función creación de boletas de Lotería
 
-
+#Creación de la función para generar sorteos
 #-----------------------------------------------------------------------------------------------------
-#Creando funcion para comparar boletos
-def comparar_boletas(sorteo):
-    print("\nComparación de Boletas:")
-    #Búcle para la comparación de boleta
-    for i, boleta in enumerate(boleta_total):
-        coincidencias = set(boleta) & set(sorteo)  # Comparamos cada boleta con el sorteo
-        if coincidencias:
-            print(f"Boleta {i + 1}: Le pegaste a {len(coincidencias)} número(s), coincidencias: {sorted(coincidencias)}") #Compara las coindicencias
-        else:
-            print(f"Boleta {i + 1}: No ganó.")
+def creacion_sorteos():
+    
+ #Creación de la función secundaria para comparar boletas     
+#------------------------------------------------------------------------------------------------------   
+    #Creando funcion para comparar boletos
+    def comparar_boletas(sorteo,total_boletas):
 
-    
+        #Búcle para la comparación de boleta
+        for i, boleta in enumerate(total_boletas):
+            coincidencias = set(boleta) & set(sorteo)  # Comparamos cada boleta con el sorteo
+            if coincidencias:
+                print(f"Boleta {i + 1}: Le pegaste a {len(coincidencias)} número(s), coincidencias: {sorted(coincidencias)}") #Compara las coindicencias
+            else:
+                print(f"Boleta {i + 1}: No ganó.")
 #------------------------------------------------------------------------------------------------------
-    
-#generar numero random en sorteos
-def generar_sorteo():
-    return random.sample(range(0,66),6)
-    
-    #Fin de generar sorteo random
+#fin de la función secundaria para generar sorteos para Loteria 
+
+#Creación de la función secundaria para generar sorteos para Loteria      
+#------------------------------------------------------------------------------------------------------
+        
+    #Creando función para generar numeros randoms para los sorteos
+    def generar_sorteo_loteria():
+       
+        #Listas para el Sorteo:
+        total_sorteos_loteria = []
+
+        #generar numero random en sorteos
+        sorteo_loteria = random.sample(range(0,66),6)
+        
+        total_sorteos_loteria.append(sorteo_loteria)
+        return sorteo_loteria
+        
+        
+        #Fin de generar sorteo random
 #----------------------------------------------------------------------------------------------------
+#Fin de la función secundaria para generar sorteos para Loteria   
+
+#Creación de la función secundaria para generar sorteos para Saca3   
+#----------------------------------------------------------------------------------------------------------------
+    def generar_sorteo_saca3():
+
+        #Listas para el Sorteo Saca 3
+        total_sorteos_saca3 = []
+
+        #generar numero random en sorteos utilizando Choises para que puedan repetirse:
+        sorteo_saca3 = random.choices(range(0,9), k=3) #La k = 3 es para indicar cuantos números deberá de generar
+        total_sorteos_saca3.append(sorteo_saca3)
+        return sorteo_saca3
+
+    return comparar_boletas, generar_sorteo_loteria, generar_sorteo_saca3, 
     
-    
-    
+#----------------------------------------------------------------------------------------------------------------   
     #Manejando función para la creación de archivos
 #------------------------------------------------------------------------------
 
@@ -191,58 +237,65 @@ def menu():
 def main():
     #Referencias de funciones:
     principal, juego, boletas = menu()
-    aleatorio, manual = creacion_boletas_loteria()
+    aleatorio, manual, total_boletas = creacion_boletas_loteria()
+    comparar, generarloteria, generarsaca3  = creacion_sorteos()
+    
     while True:
         try:
             principal()
             opcion = str(input("\n Elige la opción a seleccionar: "))
-            
-
+           
             match opcion:
 
                 #Lotería
                 case "1":
                     # Llamando la función Limpiar_pantalla
                     limpiar_pantalla()
+                    
                     while True:
-                        try:
-                            # Llamando la función menu_loteria
-                            juego()
-                            opcion_uno = str(input("\n Elige la opción a seleccionar: "))
+                        
+                        # Llamando la función menu_loteria
+                        juego()
+                        opcion_loteria = str(input("\n Elige la opción a seleccionar: "))
+                        
+                        match opcion_loteria:
+                            case "1":
+                                '''
+                                Jorge, esta parte te la dejaré a ti, lo que debes de hacer es implementar el menu donde indique si el usuario quiere jugar manual o automático.
+                                Inténtalo realizarlo con un while true y con un try y except. Para implementar el menu para boletas, solo tienes que llamarla: boletas()
+                                '''
+                                # Llamando la función Limpiar_pantalla
+                                limpiar_pantalla()
+                                manual()
+                                
+                                    
+                            case "2":
+                                print(f"\n {"-" * 15} Sorteos {"-" * 15}\n")
+                                
+                                print(f"\n {"-" * 7}Comparación de Boletas: {"-" * 7}")   
+                                #Llamando la función generar_boletas()
+                                sorteo = generarloteria()
 
-                            match opcion_uno:
-                                case "1":
-                                    '''
-                                    Jorge, esta parte te la dejaré a ti, lo que debes de hacer es implementar el menu donde indique si el usuario quiere jugar manual o automático.
-                                    Inténtalo realizarlo con un while true y con un try y except. Para implementar el menu para boletas, solo tienes que llamarla: boletas()
-                                    '''
-                                    # Llamando la función Limpiar_pantalla
-                                    limpiar_pantalla()
-                                    manual()
-                                    sorteo = generar_sorteo()
-                                    print(f"El sorteo es: {sorteo}")
-                                    
-                                case "2":
-                                    print(f"\n {"-" * 15} Sorteos {"-" * 15}\n")
-                                    juego()
-                                    comparar_boletas(sorteo)
+                                print(f"\n El sorteo es: {sorteo} \n")
+
+                                #Llamando la función comparar_sorteos()
+                                comparar(sorteo,total_boletas)
                                     
                                     
                                     
-                                case "3":
-                                     print(f"\n {"-" * 15} Registro {"-" * 15}\n")
+                            case "3":
+                                print(f"\n {"-" * 15} Registro {"-" * 15}\n")
                                      
-                                     print("Le vamos a crear un archivo con las boletas\n")
+                                print("Le vamos a crear un archivo con las boletas\n")
                                      
-                                     escoger()#llamo a la función para que escoja lo que desea hacer
+                                escoger()#llamo a la función para que escoja lo que desea hacer
                                      
-                                case "4":
-                                    print(f"{"-" * 15} Saliendo del programa... {"-" * 15}")
-                                    break
-                                case _:
+                            case "4":
+                                print(f"{"-" * 15} Saliendo del programa... {"-" * 15}")
+                                break
+                            case _:
                                     print("Hubo un error al intentar seleccionar la opción deseada.")
-                        except Exception as error:
-                            print(f"Tiene un error llamado {error}, inténtalo más tarde por favor.")
+                        
                     
                     
 
@@ -251,17 +304,71 @@ def main():
                     # Llamando la función Limpiar_pantalla
                     limpiar_pantalla()
                     print(f"\n {"-" * 15} Saca 3 {"-" * 15}\n")
+
                     # Llamando la función menu_saca3
                     juego()
+                    opcion_saca3 = str(input("\n Elige la opción a seleccionar: "))
 
+                    match opcion_saca3:
+                        case "1":
+                            # Llamando la función Limpiar_pantalla
+                            limpiar_pantalla()
+
+                            boletas()
+                            opcion_boletas = str(input("\n Elige la opción a seleccionar: "))
+
+                            match opcion_boletas:
+                                case "1":
+                                    
+                                    # Llamando la función Limpiar_pantalla
+                                    limpiar_pantalla()
+                                    
+                                    print(f"\n {"-" * 15} Creación de Boletas Automaticas: {"-" * 15}\n")
+
+                                case "2":
+                                    # Llamando la función Limpiar_pantalla
+                                    limpiar_pantalla()
+
+                                    print(f"\n {"-" * 15} Creación de Boletas Manuales: {"-" * 15}\n")
+                                    
+                                case _:
+                                    print("Hubo un error al intentar seleccionar la opción deseada.")
+
+                        case "2":
+
+                            # Llamando la función Limpiar_pantalla
+                            limpiar_pantalla()
+
+                            print(f"\n {"-" * 15} Sorteos {"-" * 15}\n")
+                                
+                            print(f"\n {"-" * 7}Comparación de Boletas: {"-" * 7}")   
+                            #Llamando la función generar_boletas()
+                            sorteo = generarsaca3()
+
+                            print(f"\n El sorteo es: {sorteo} \n")
+
+                            
+                            
+                        case "3":
+                            # Llamando la función Limpiar_pantalla
+                            limpiar_pantalla()
+                        case "4":
+                            print(f"{"-" * 15} Saliendo del programa... {"-" * 15}")
+                            break
+                        case _:
+                            print("Hubo un error al intentar seleccionar la opción deseada.")
                 #Salir del programa
                 case "3":
                     print(f"{"-" * 15} Saliendo del programa... {"-" * 15}")
                     break
                 case _:
                     print("Hubo un error al intentar seleccionar la opción deseada.")
-        except:
-            print("Hubo error en el programa por lo que no pudo ser compilado.")
+        except ValueError as value:
+                print(f"\n Ingresaste un valor diferente a lo que se pide, por favor, inténtelo de nuevo. Error: {value} ")
+        except Exception as error:
+                            print(f"Tiene un error llamado {error}, inténtalo más tarde por favor.")
+#Returns de la función main:
+    
 # Fin de la función Main
 #--------------------------------------------------------------------------------------------
 #Llamada a la función Main para que el programa empiece con dicha función
